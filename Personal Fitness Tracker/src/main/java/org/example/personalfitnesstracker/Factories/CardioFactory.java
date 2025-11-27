5f64a724debcbb700f9b8c0c0426f03efb0422b2
B.Bhuiyan <143551642+Boohuan@users.noreply.github.com>
2025-11-27 12:55:01 -0500

    fixed errors in cardio and muscular factory
    since userId and dateStamp weren't ordered properly as ordered in Workout parent classes, which didn't let the app compile and run

--- Personal Fitness Tracker/src/main/java/org/example/personalfitnesstracker/Factories/CardioFactory.java
+++ Personal Fitness Tracker/src/main/java/org/example/personalfitnesstracker/Factories/CardioFactory.java
@@ -6,6 +6,6 @@ import org.example.personalfitnesstracker.Models.Workout;
 public class CardioFactory implements IWorkoutFactory{
     @Override
     public Workout addNewWorkoutSession(WorkoutAttributeData attr) {
-        return new CardioWorkout(attr.workoutId(), attr.workoutName(), attr.workoutDescription(), attr.workoutDuration(), attr.caloriesBurned(), attr.dateStamp(), attr.userId(), attr.totalDistance(), attr.heartRateZone());
+        return new CardioWorkout(attr.workoutId(), attr.workoutName(), attr.workoutDescription(), attr.workoutDuration(), attr.caloriesBurned(),attr.userId(), attr.dateStamp(), attr.totalDistance(), attr.heartRateZone());
     }
 }
--- Personal Fitness Tracker/src/main/java/org/example/personalfitnesstracker/Factories/MuscularFactory.java
+++ Personal Fitness Tracker/src/main/java/org/example/personalfitnesstracker/Factories/MuscularFactory.java
@@ -6,6 +6,6 @@ import org.example.personalfitnesstracker.Models.Workout;
 public class MuscularFactory implements IWorkoutFactory{
     @Override
     public Workout addNewWorkoutSession(WorkoutAttributeData attr) {
-        return new MuscularWorkout(attr.workoutId(), attr.workoutName(), attr.workoutDescription(), attr.workoutDuration(), attr.caloriesBurned(), attr.dateStamp(), attr.userId(), attr.totalSets(), attr.totalReps(), attr.totalWeight());
+        return new MuscularWorkout(attr.workoutId(), attr.workoutName(), attr.workoutDescription(), attr.workoutDuration(), attr.caloriesBurned(), attr.userId(), attr.dateStamp(), attr.totalSets(), attr.totalReps(), attr.totalWeight());
     }
 }
